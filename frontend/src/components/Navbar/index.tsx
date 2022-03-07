fa6974733f835464b36d4245fbb9baa4e231dd2c
Yan Brendon <yanbrendon@hotmail.com>
2022-03-07 13:34:00 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GitHubIcon } from 'assets/img/github.svg';
+
+import './style.css';
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-contant">
+                    <h1 className="dsmovie-contact-title">DSMovie</h1>
+                    <a href="https://github.com/YanBrendon" >
+                        <div className="dsmovie-contact-container">
+                            <GitHubIcon />
+                            <p className="dsmovie-contatc-link">/YanBrendon</p>
+                        </div></a>
+                </div>
+            </nav>
+        </header>)
+}
+
+
+
+export default Navbar
